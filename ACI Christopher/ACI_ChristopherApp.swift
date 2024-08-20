a092e4fd43dfd038f1d08fedac8b19cf4e860aaf
ajizlhkm <ajiztvm2@gmail.com>
2024-08-20 12:07:42 +0700

    Initial Commit

---
+++ ACI Christopher/ACI_ChristopherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACI_ChristopherApp.swift
+//  ACI Christopher
+//
+//  Created by Aji Zul Hakim on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ACI_ChristopherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ACI Christopher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ACI Christopher
+//
+//  Created by Aji Zul Hakim on 20/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
